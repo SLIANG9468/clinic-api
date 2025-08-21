9acc20b3336d9834e48bee9b030ca08b29b3fe21
Sherri Liang-Zhou <sherriliang-zhou@MacBook-Pro-3.local>
2025-08-20 19:41:20 -0500

    updated route

--- app.py
+++ app.py
@@ -89,18 +89,55 @@ customer_schema = CustomerSchema() #Creating an instance of my schema that I can
 
 @app.route('/customers', methods=['POST']) #route servers as the trigger for the function below.
 def create_customer():
-    # try:
-    #     data = customer_schema.load(request.json)
-    # except ValidationError as e:
-    #     return jsonify(e.messages) #Returning the error as a response so my client can see whats wrong.
+    try:
+        data = customer_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400 #Returning the error as a response so my client can see whats wrong.
     
-    print("My Translated Data")
-    #print(data)
-    # new_customer = Customers(**data) #Creating User object
-    # db.session.add(new_customer)
-    # db.session.commit()
-    return "Creating a customer"
+    new_customer = Customers(**data) #Creating User object
+    db.session.add(new_customer)
+    db.session.commit()
+    return customer_schema.jsonify(new_customer), 201 
 
+#read Customers
+@app.route('/customers', methods=['GET'])
+def read_customers():
+    customers = db.session.query(Customers).all()
+    return customer_schema.jsonify(customers, many=True), 200
+
+#read individual customer
+@app.route('/customers/<int:customer_id>', methods=['GET'])
+def read_customer(customer_id):
+    customer = db.session.get(Customers, customer_id)
+    return customer_schema.jsonify(customer), 200
+
+#Delete a customer
+@app.route('/customers/<int:customer_id>', methods=['DELETE'])
+def delete_customer(customer_id):
+    customer = db.session.get(Customers, customer_id)
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({"message": f"Successfully deleted customer {customer_id}"}), 200
+
+
+#Update a customer
+@app.route('/customers/<int:customer_id>', methods=['PUT'])
+def update_customer(customer_id):
+    customer = db.session.get(Customers, customer_id) #Query for our customer to update
+
+    if not customer: #Checking if I got a customer
+        return jsonify({"message": "customer not found"}), 404  #if not return error message
+    
+    try:
+        customer_data = customer_schema.load(request.json) #Validating updates
+    except ValidationError as e:
+        return jsonify({"message": e.messages}), 400
+    
+    for key, value in customer_data.items(): #Looping over attributes and values from user data dictionary
+        setattr(customer, key, value) # setting Object, Attribute, Value to replace
+
+    db.session.commit()
+    return customer_schema.jsonify(customer), 200
 
 with app.app_context():
     db.create_all() #Creating our database tables
